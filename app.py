054e783ff97e9b4cfdeb4f11d24a3b6185b73102
Trishane Reid <trishanereid17@gmail.com>
2026-05-06 23:25:51 +0530

    feat: setup streamlit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='QuantCore',
+    page_icon='chart_with_upwards_trend',
+    layout='wide'
+)
+
+st.title('QuantCore')
+st.markdown('A quantitative finance engine built from mathematical first principles.')
+st.markdown('Select a tool from the sidebar to get started.')
